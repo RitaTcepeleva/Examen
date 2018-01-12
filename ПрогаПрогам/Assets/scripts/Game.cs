d32a97258bac9a94376264b445f7f233f3eb41d6
Rita Tcepeleva <tsepeleva.rita@mail.ru>
2018-01-12 20:10:40 +0300

    first commit

---
+++ ПрогаПрогам/Assets/scripts/Game.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+
+	public Text moneyText;
+	private int money;
+	public Image bgImg;
+	public Image[] bttnImgs= new Image[2];
+
+	private void Start () {
+	
+		PurchaseManager.OnPurchaseNonConsumable += PurchaseManager_OnPurchaseNonConsumable;
+	
+		PurchaseManager.OnPurchaseConsumable += PurchaseManager_OnPurchaseConsumable;
+	}
+
+	void PurchaseManager_OnPurchaseConsumable (PurchaseEventArgs args)
+	{
+		money += 1000;
+		moneyText.text = "ВАЛЮТА: " + money + "$";
+		Debug.Log ("You purchse:" + args.purchasedProduct.definition.id + "- Consumable");
+	}
+
+	void PurchaseManager_OnPurchaseNonConsumable (PurchaseEventArgs args)
+	{
+		bgImg.color = new Color (227 / 255.0f, 41 / 255.0f, 128 / 255.0f);
+			foreach (Image i in bttnImgs) i.color = new Color (89 / 255.0f, 41 / 255.0f, 160 / 255.0f);
+
+		Debug.Log ("You purchse:" + args.purchasedProduct.definition.id + "- Non Consumable");
+	}
+}
